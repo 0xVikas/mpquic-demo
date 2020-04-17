f0c9d9db76b24ff65452d4586827c60fe4482a27
Vikas <vikasrvs@gmail.com>
2020-04-17 14:28:45 +0530

    Print requests at client side

--- client.go
+++ client.go
@@ -4,17 +4,38 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	quic "github.com/lucas-clemente/quic-go"
 
 	"github.com/lucas-clemente/quic-go/h2quic"
-//	"github.com/lucas-clemente/quic-go/internal/utils"
 )
+
+// Function for printing the HTTP Request
+func formatRequest(r *http.Request) string {
+	var request []string
+	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
+	request = append(request, url)
+	request = append(request, fmt.Sprintf("Host: %v", r.Host))
+	for name, headers := range r.Header {
+	  name = strings.ToLower(name)
+	  for _, h := range headers {
+		request = append(request, fmt.Sprintf("%v: %v", name, h))
+	  }
+	}
+
+	if r.Method == "POST" {
+	   r.ParseForm()
+	   request = append(request, "\n")
+	   request = append(request, r.Form.Encode())
+	} 
+	 return strings.Join(request, "\n")
+}
 
 func init(){
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-//		utils.SetLogTimeFormat("")
+		fmt.Println(formatRequest(r))
 
 		quicConfig := &quic.Config{
 			CreatePaths: true,
@@ -26,20 +47,18 @@ func init(){
 
 		addr := "https://localhost:6121" + r.URL.Path
 
-//		utils.Infof("GET %s", addr)
+		rsp, err := hclient.Get(addr)
+		if err != nil {
+			panic(err)
+		}
 
-			rsp, err := hclient.Get(addr)
-			if err != nil {
-				panic(err)
-			}
-//			utils.Infof("Got response for %s: %#v", addr, rsp)
+		body, err := ioutil.ReadAll(rsp.Body)
 
-			body, err := ioutil.ReadAll(rsp.Body)
+		if err != nil {
+			fmt.Printf("error reading body while handling /echo: %s\n", err.Error())
+		}
 
-			if err != nil {
-				fmt.Printf("error reading body while handling /echo: %s\n", err.Error())
-			}
-			w.Write(body)
+		w.Write(body)
 
 	})
 }
